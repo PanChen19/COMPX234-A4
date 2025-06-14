8ada4a8a9a1dab33601d6ebee9d7adcd4499784d
PanChen <2528376371@qq.com>
2025-06-14 17:56:44 +0800

    4.Add the code of the request of read or send request of part of the file

--- client.py
+++ client.py
@@ -8,11 +8,12 @@ client = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 # Request to doenload file
 filename = "file.txt"
 download_message = f"DOWNLOAD {filename}"
-
-#Send some data
-client.sendto('AAABBBCCC'.encode(),(target_host,target_port))
- 
+#Request to download part of the file
+start_byte = 0
+end_byte = 1023
+file_request = f"FILE example.txt GET START {start_byte} END {end_byte}"
+socket.sendto(file_request.encode(), (target_host,target_port))
 #Receive some data
-data,addr = client.recvfrom(4096)
+data,addr = client.recvfrom(1024)
  
 print(data.decode())
--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import socket
 import threading
 import os 
+import base64
+
 bind_ip = '0.0.0.0'
 bind_port = 9999
 
@@ -9,6 +11,22 @@ server = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
  
 #Configure the listening IP and port
 server.bind((bind_ip,bind_port))
+filename = "file.txt"
+with open(filename, 'rb') as file:
+    while True:
+        # Receive the request
+        data, client_address = server.recvfrom(1024)
+        request = data.decode()
+        if request.startswith('FILE'):
+            # decode
+            _, file_name, _, start, _, end = request.split()
+            start_byte = int(start)
+            end_byte = int(end)
+            
+            # read the special part
+            file.seek(start_byte)
+            file_data = file.read(end_byte - start_byte + 1)
+            encoded_data = base64.b64encode(file_data).decode()
 
 def handle_client():
     #If get the data
